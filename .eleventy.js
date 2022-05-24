d569e673fa9bd56a2cc4152c2bb1886455cc1988
DanyDeB <104914575+DanyDeB@users.noreply.github.com>
2022-05-24 20:46:33 +0200

    fixed eleventy

--- .eleventy.js
+++ .eleventy.js
@@ -4,7 +4,7 @@ module.exports = function(elventyConfig) {
 
     elventyConfig.addPassthroughCopy('./src/style.css');
     elventyConfig.addPassthroughCopy('./src/assets');
-    elventyConfig.addPassthroughCopy('./src/admin');
+    elventyConfig.addPassthroughCopy('./src/admin'); 
     
     elventyConfig.addFilter("postDate", (dateObj) => {
     return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
