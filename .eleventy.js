b33c7010d48b7c0bad3ebec20f7fc696026e2cf8
DanyDeB <104914575+DanyDeB@users.noreply.github.com>
2022-05-24 20:23:45 +0200

    mixed admin panel

--- .eleventy.js
+++ .eleventy.js
@@ -3,7 +3,8 @@ const { DateTime } = require("luxon");
 module.exports = function(elventyConfig) {
 
     elventyConfig.addPassthroughCopy('./src/style.css');
-    elventyConfig.addPassthroughCopy('./src/assets')
+    elventyConfig.addPassthroughCopy('./src/assets');
+    elventyConfig.addPassthroughCopy('./src/admin');
     
     elventyConfig.addFilter("postDate", (dateObj) => {
     return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
